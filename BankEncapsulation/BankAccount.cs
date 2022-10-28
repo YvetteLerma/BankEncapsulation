a944e6b061727b6b38c5856db138d200cc7aac02
Yvette Lerma <lerma.yvette@hotmail.com>
2022-10-28 13:19:57 -0600

    code added

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount(double amount)
+        {
+            _balance = amount;
+        }
+
+        private double _balance = 0;
+
+        //define a method named deposit that will accept a double
+        //and store that value in the balance field
+
+        public void Deposit(double amount)
+        {
+            _balance += amount;
+        }
+
+        public void Withdraw( double amount)
+        {
+            _balance -= amount;
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+    }
+}
+
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace BankEncapsulation
+﻿using System.Security.Principal;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var yvettesAccount = new BankAccount(100);
+
+            Console.WriteLine($"Current Balance: ${yvettesAccount.GetBalance()}");
+
+            yvettesAccount.Deposit(200);
+
+            Console.WriteLine($"After your deposit, your current balance is ${yvettesAccount.GetBalance()}");
+
+            yvettesAccount.Withdraw(150);
+
+            Console.WriteLine($"After your withdraw, your current balance is ${yvettesAccount.GetBalance()}");
         }
+    
+
     }
 }
